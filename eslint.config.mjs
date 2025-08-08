89083b443ef889a044a52484d4a67c797284c12e
심유진 <syj11243@naver.com>
2025-08-08 23:06:45 +0900

    fix : eslint 설정 수정 (#17)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,11 @@ import storybook from 'eslint-plugin-storybook';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
-import filenames from 'eslint-plugin-filenames';
 import importPlugin from 'eslint-plugin-import';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import nextPlugin from '@next/eslint-plugin-next';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,41 +18,37 @@ const compat = new FlatCompat({
 const eslintConfig = [
   {
     languageOptions: {
-      parser: '@typescript-eslint/parser',
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      next: nextPlugin,
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
     },
     rules: {
-      '@next/next/no-img-element': 'error',
+      'next/no-img-element': 'error',
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/explicit-module-boundary-types': 'warn',
+      'prettier/prettier': 'error',
     },
   },
   ...storybook.configs['flat/recommended'],
   {
     plugins: {
-      filenames,
       import: importPlugin,
     },
     rules: {
-      'filenames/match-regex': ['error', '^[a-z0-9-]+$', true],
-      'filenames/match-exported': ['error', 'pascalCase'],
       'import/order': [
         'error',
         {
-          groups: [
-            ['builtin', 'external'],
-            ['internal'],
-            ['sibling', 'parent'],
-            ['index'],
-          ],
+          groups: [['builtin', 'external'], ['internal'], ['sibling', 'parent'], ['index']],
           'newlines-between': 'always',
         },
       ],
-    },
-  },
-  {
-    plugins: ['@typescript-eslint', 'prettier'],
-    rules: {
-      'prettier/prettier': 'error',
     },
   },
 ];
