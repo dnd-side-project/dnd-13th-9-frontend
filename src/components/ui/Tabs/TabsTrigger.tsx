6c9591280c4394e404d67620a786d189be7c4020
심유진 <syj11243@naver.com>
2025-08-09 14:09:49 +0900

    style : 색상 및 텍스트 스타일 수정 (#19)

--- src/components/ui/Tabs/TabsTrigger.tsx
+++ src/components/ui/Tabs/TabsTrigger.tsx
@@ -16,10 +16,10 @@ export function TabsTrigger({ value, children }: TabsTriggerProps) {
   return (
     <button
       className={cn(
-        'flex-1 px-6 py-3.5 pb-2 font-semibold',
+        'h flex-1 px-6 py-3.5 pb-2 font-semibold whitespace-nowrap',
         isActive
           ? 'border-b-3 border-blue-500 text-black'
-          : 'border-b-3 border-b-transparent text-neutral-400'
+          : 'border-b-3 border-b-transparent text-[#A4A4A4]'
       )}
       onClick={() => setActiveTab(value)}
     >
