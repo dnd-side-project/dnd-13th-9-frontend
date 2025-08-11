ed97e27d10738790cbf8a8da44a40275031f204c
심유진 <syj11243@naver.com>
2025-08-11 15:09:11 +0900

    feat : Button 컴포넌트 제작 (#21)

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,43 +1,68 @@
+import { TitleXs } from '../Typography';
+import { cn } from '@/utils/utils';
+import { ReactNode } from 'react';
+
+type Variant = 'primary' | 'secondary' | 'tertiary';
+type Size = 'small' | 'medium' | 'large';
+
 export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
-  /** How large should the button be? */
-  size?: 'small' | 'medium' | 'large';
-  /** Button contents */
-  label: string;
-  /** Optional click handler */
+  variant?: Variant;
+  size?: Size;
+  label?: string;
   onClick?: () => void;
+  disabled?: boolean;
+  children?: ReactNode;
+  loading?: boolean;
+  className?: string;
 }
 
-/** Primary UI component for user interaction */
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
+  variant = 'primary',
+  size = 'small',
   label,
+  children,
+  disabled = false,
+  loading = false,
+  className,
   ...props
 }: ButtonProps) => {
   const baseClass =
-    'inline-flex items-center justify-center rounded-lg text-white font-semibold';
-  const sizeClass = {
-    small: 'px-3 py-1 text-sm',
-    medium: 'px-4 py-2 text-base',
-    large: 'px-6 py-3 text-lg',
+    'inline-flex items-center justify-center rounded-xl transition-colors duration-200';
+
+  const sizeClass: Record<Size, string> = {
+    small: 'px-7 py-3 max-w-[120px]',
+    medium: 'px-12 py-3 min-w-[150px]',
+    large: 'px-36 py-4 min-w-[310px]',
   };
-  const modeClass = primary
-    ? 'bg-blue-500 hover:bg-blue-600'
-    : 'bg-gray-500 hover:bg-gray-600';
+
+  const variantClass: Record<Variant, string> = {
+    primary: 'bg-primary-50 hover:bg-primary-55 text-white',
+    secondary: 'bg-coolGray-80 hover:bg-neutral-90 text-white',
+    tertiary: 'bg-neutral-40 hover:bg-neutral-50 text-neutral-90',
+  };
+
+  const disabledClass = 'opacity-50 cursor-not-allowed hover:none';
 
   return (
     <button
       type="button"
-      className={`${baseClass} ${sizeClass[size]} ${modeClass}`}
-      style={{ backgroundColor }}
+      disabled={disabled || disabled}
+      className={cn(
+        baseClass,
+        sizeClass[size],
+        variantClass[variant],
+        (disabled || loading) && disabledClass,
+        'gap-2',
+        className
+      )}
       {...props}
     >
-      {label}
+      <TitleXs className="inline-flex items-center gap-2 whitespace-nowrap">
+        {loading && (
+          <span className="h-4 w-4 animate-spin rounded-full border-2 border-white border-t-transparent" />
+        )}
+        {children ?? label}
+      </TitleXs>
     </button>
   );
 };
