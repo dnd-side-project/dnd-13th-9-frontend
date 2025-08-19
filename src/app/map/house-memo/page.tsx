534f4660c18a2872d5c52109eb14a2d76946a087
심유진 <syj11243@naver.com>
2025-08-19 22:53:06 +0900

    fix : Tabs 컴포넌트 수정 (#23)

--- src/app/map/house-memo/page.tsx
+++ src/app/map/house-memo/page.tsx
@@ -5,6 +5,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/Tabs';
 import BaseInfo from '@/components/HouseMemo/BaseInfo/BaseInfo';
 import { AddImgButtonGroup } from '@/components/HouseMemo/AddImgGroup';
 import { HouseMemoContext, initialHouseMemo } from '@/contexts/HouseMemoContext';
+import CheckList from '@/components/HouseMemo/CheckList/CheckList';
 
 export default function page() {
   const [houseMemo, setHouseMemo] = useState(initialHouseMemo);
@@ -15,14 +16,16 @@ export default function page() {
         <AddImgButtonGroup />
         <Tabs defaultValue="baseInfo">
           <TabsList>
-            <TabsTrigger value="baseInfo">기본 정보</TabsTrigger>
-            <TabsTrigger value="checkList">체크 리스트</TabsTrigger>
+            <TabsTrigger.Bar value="baseInfo">기본 정보</TabsTrigger.Bar>
+            <TabsTrigger.Bar value="checkList">체크 리스트</TabsTrigger.Bar>
           </TabsList>
 
           <TabsContent value="baseInfo">
             <BaseInfo />
           </TabsContent>
-          <TabsContent value="checkList">content2</TabsContent>
+          <TabsContent value="checkList">
+            <CheckList />
+          </TabsContent>
         </Tabs>
       </HouseMemoContext.Provider>
     </MainLayout>
