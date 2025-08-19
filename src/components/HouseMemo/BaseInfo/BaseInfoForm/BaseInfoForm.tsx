ee96b60dafb462f08899339ba66df92d75d757a8
심유진 <syj11243@naver.com>
2025-08-19 19:09:09 +0900

    feat : BaseInfo 컴포넌트 개발 (#23)

---
+++ src/components/HouseMemo/BaseInfo/BaseInfoForm/BaseInfoForm.tsx
@@ -0,0 +1,114 @@
+'use client';
+import React, { useRef } from 'react';
+import { LabelContainer } from '../../LabelContainer';
+import { Icon } from '@/components/ui/Icon';
+import { Input } from '@/components/ui/Input';
+import { ChipGroup } from '@/components/ui/ChipGroup';
+import { CurrentLocationButton } from '@/components/ui/CurrentLocationButton';
+import { KakaoMap } from '../Map';
+import { useHouseMemo } from '@/contexts/HouseMemoContext';
+import {
+  feelingOptions,
+  InputFields,
+  contractOptions,
+  houseOptions,
+  doubleInputFields,
+} from './BaseInfoConfig';
+import { ContractType, HouseType } from '@/types/house-memo';
+
+export default function BaseInfoForm() {
+  const { houseMemo, setHouseMemo } = useHouseMemo();
+  const mapRef = useRef<any>(null);
+
+  const handleFieldChange = <T extends keyof typeof houseMemo>(
+    key: T,
+    value: (typeof houseMemo)[T]
+  ) => {
+    setHouseMemo({ ...houseMemo, [key]: value });
+  };
+
+  const handleMoveToCurrentLocation = () => {
+    if (mapRef.current) {
+      mapRef.current.moveToCurrentLocation();
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-8 py-5 pb-24">
+      {/* 전반적인 느낌 */}
+      <LabelContainer label="전반적인 느낌">
+        <div className="flex max-w-20 gap-2">
+          {feelingOptions.map(({ type, iconName, activeColor }) => (
+            <Icon
+              key={type}
+              onClick={() => handleFieldChange('feeling', type)}
+              width={40}
+              height={40}
+              color={houseMemo.feeling === type ? activeColor : 'neutral'}
+              name={iconName}
+            />
+          ))}
+        </div>
+      </LabelContainer>
+
+      {/* 계약 형태 */}
+      <LabelContainer label="계약 형태" required={true}>
+        <div className="flex gap-2">
+          <ChipGroup
+            options={contractOptions}
+            value={houseMemo.contractType}
+            onChange={(val) => handleFieldChange('contractType', val as ContractType)}
+          />
+        </div>
+      </LabelContainer>
+
+      {/* 집 유형 */}
+      <LabelContainer label="집 유형">
+        <div className="flex flex-wrap gap-2">
+          <ChipGroup
+            options={houseOptions}
+            value={houseMemo.houseType}
+            onChange={(val) => handleFieldChange('houseType', val as HouseType)}
+          />
+        </div>
+      </LabelContainer>
+
+      <LabelContainer label="보증금">
+        <div className="flex gap-2">
+          {doubleInputFields.map(({ key, placeholder, unit }) => (
+            <Input
+              placeholder={placeholder}
+              value={houseMemo[key] !== undefined ? String(houseMemo[key]) : ''}
+              onChange={(e) => handleFieldChange(key, e.target.value)}
+              unit={unit}
+              className="max-w-47"
+            />
+          ))}
+        </div>
+      </LabelContainer>
+
+      {InputFields.map(({ key, label, placeholder, required, unit }) => (
+        <LabelContainer key={key} label={label} required={required}>
+          {key === 'address' ? (
+            <>
+              <CurrentLocationButton onClick={handleMoveToCurrentLocation} />
+              <Input
+                placeholder={placeholder}
+                value={houseMemo.address || ''}
+                onChange={(e) => handleFieldChange('address', e.target.value)}
+              />
+              <KakaoMap ref={mapRef} height="130px" />
+            </>
+          ) : (
+            <Input
+              placeholder={placeholder}
+              value={houseMemo[key] as string}
+              onChange={(e) => handleFieldChange(key, e.target.value)}
+              unit={unit}
+            />
+          )}
+        </LabelContainer>
+      ))}
+    </div>
+  );
+}
