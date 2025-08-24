a4c16fb18de14d696f8481bb4c75188ad1a3e7b1
심유진 <syj11243@naver.com>
2025-08-25 03:42:18 +0900

    feat : CheckList Box 컴포넌트 제작 (#49)

---
+++ src/components/ui/CheckListBox/CheckListTitle.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Icon } from '../Icon';
+import { TitleM } from '../Typography';
+import { IconName } from '../assets';
+
+type Props = {
+  iconName?: IconName;
+  title: string;
+  className?: string;
+};
+
+export function CheckListTitle({ iconName = 'example', title, className }: Props) {
+  return (
+    <div className={`flex gap-3 px-7 py-4 pt-6 ${className || ''}`}>
+      <Icon name={iconName} />
+      <TitleM>{title}</TitleM>
+    </div>
+  );
+}
