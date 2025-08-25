f56bbfd3115fe55c4d60b4b6518863f345747f9f
심유진 <syj11243@naver.com>
2025-08-25 19:50:45 +0900

    feat : 쿼리 무효화 로직 추가 (#57)

--- src/queries/user/useLogout.ts
+++ src/queries/user/useLogout.ts
@@ -1,14 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { logout } from '@/services/auth';
 import { LogoutResponse } from '@/types/auth';
 import { useRouter } from 'next/navigation';
 
 export function useLogout() {
   const router = useRouter();
-
+  const queryClient = useQueryClient();
   return useMutation<LogoutResponse, Error, void>({
     mutationFn: logout,
-    onSuccess: () => router.push('/login'),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['myInfo'] });
+      router.push('/login');
+    },
     onError: (err) => console.error('로그아웃 실패', err),
   });
 }
