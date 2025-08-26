e95992ebc16c874eaab2e8a1f0b1a29b5c408fb1
심유진 <syj11243@naver.com>
2025-08-26 13:56:54 +0900

    feat : 헤더 완료 버튼 추가 (#58)

--- src/app/map/house-memo/page.tsx
+++ src/app/map/house-memo/page.tsx
@@ -14,6 +14,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { Header } from '@/components/ui/Header';
 import { Icon } from '@/components/ui/Icon';
+import { TitleS } from '@/components/ui/Typography';
 
 export default function page() {
   const { isOpen, closeModal, openModal } = useModal();
@@ -43,6 +44,7 @@ export default function page() {
             />
           }
           title="매물 메모"
+          right={<TitleS className="text-primary-50 px-3 whitespace-nowrap">완료</TitleS>}
         />
         <AddImgButtonGroup />
         <Tabs defaultValue="baseInfo">
