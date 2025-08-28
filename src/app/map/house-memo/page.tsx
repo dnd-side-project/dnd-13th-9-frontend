f15ae17628201e7eaf06b516edf982f8fb7c27de
심유진 <syj11243@naver.com>
2025-08-28 14:53:02 +0900

    feat : 체크리스트 스크롤 (#45)

--- src/app/map/house-memo/page.tsx
+++ src/app/map/house-memo/page.tsx
@@ -91,7 +91,7 @@ function HouseMemoContent() {
       />
       <AddImgButtonGroup storageKey="images" />
       <Tabs value={activeTab} onValueChange={setActiveTab}>
-        <div className="px-6">
+        <div className="sticky top-0 z-50 w-full bg-white px-6">
           <TabsList>
             <TabsTrigger.Bar value="baseInfo">기본 정보</TabsTrigger.Bar>
             <TabsTrigger.Bar value="checkList">체크리스트</TabsTrigger.Bar>
