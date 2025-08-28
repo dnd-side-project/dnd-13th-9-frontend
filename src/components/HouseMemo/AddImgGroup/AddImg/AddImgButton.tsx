d8a262de99644e0d951ce4f44276ea4dfce6e377
심유진 <syj11243@naver.com>
2025-08-28 12:40:23 +0900

    feat : 이미지 하나만 선택할 수 있도록 (#45)

--- src/components/HouseMemo/AddImgGroup/AddImg/AddImgButton.tsx
+++ src/components/HouseMemo/AddImgGroup/AddImg/AddImgButton.tsx
@@ -56,6 +56,7 @@ export function AddImgButton({
           type="file"
           className="hidden"
           accept=".png, .jpg, .jpeg"
+          multiple={false}
           onChange={onFileChange}
         />
       )}
